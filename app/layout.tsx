5ff8874c04c7e24760b2fe6b854994610b6cd8c2
MarianoVera1979 <marianovera79@gmail.com>
2023-07-04 20:47:32 -0300

    form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,29 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Nav from './componentes/Nav'
-import Footer from './componentes/Footer'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Nav from "./componentes/Nav";
+import Footer from "./componentes/Footer";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Sitio de Perritos',
-  description: 'Practicando NextJS',
-}
+  title: "Sitio de Perritos",
+  description: "Practicando NextJS",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
-     
       <body className={inter.className}>
-
-      <div className='contenedor'>
-        <Nav />        
-        {children}
-        <Footer />
-      </div>
-        </body>
+        <div className="contenedor">
+          <Nav />
+          {children}
+          <Footer />
+        </div>
+      </body>
     </html>
-  )
+  );
 }
